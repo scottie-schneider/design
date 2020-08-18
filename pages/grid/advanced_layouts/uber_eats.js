4e49c7707f6eac6da17633627bd9c6b50989e53b
scottie-schneider <scott@laclave.co>
2020-08-18 12:46:31 -0500

    scheduling

--- pages/grid/advanced_layouts/uber_eats.js
+++ pages/grid/advanced_layouts/uber_eats.js
@@ -9,6 +9,7 @@ const StyledBody = styled.div`
   color: #262;
   display: grid;
   grid: 74px 1fr max-content / 1fr;
+  max-width: 100%;
   header,
   main,
   footer {
@@ -74,6 +75,21 @@ const StyledBody = styled.div`
     }
     #main-delivery-container {
       grid-area: main-delivery;
+      display: grid;
+      grid: repeat(2, max-content) / auto-flow minmax(max-content, 1fr) minmax(
+          max-content,
+          3fr
+        );
+      grid-gap: 8px;
+      padding-top: 16px;
+      .main-delivery-label {
+        color: #535331;
+        font-weight: 400;
+      }
+      .main-delivery-choice {
+        border: 1px solid #262;
+        padding: 8px;
+      }
     }
     #store-list-container {
       grid-area: store-list;
@@ -100,7 +116,12 @@ const UberEats = () => {
             <input type="text" placeholder="Search for a place to eat" />
           </form>
         </div>{" "}
-        <div id="main-delivery-container">Main delivery form goes here </div>
+        <div id="main-delivery-container">
+          <div className="main-delivery-label">When</div>
+          <div className="main-delivery-choice">ASAP</div>
+          <div className="main-delivery-label">To</div>
+          <div className="main-delivery-choice">123 Main St</div>
+        </div>
         <section id="store-list-container"> store list goes here</section>
       </main>
       <footer>Footer goes here</footer>
